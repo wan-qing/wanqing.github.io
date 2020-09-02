65393a9084e448d960721b520b3743e80a399dc6
JarvisHsu <xu3256622923@gmail.com>
2020-09-02 15:50:59 +0800

    SSMBOOT基本框架完成

---
+++ yiwu/src/main/java/com/hellojava/YiwuApplication.java
@@ -0,0 +1,13 @@
+package com.hellojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiwuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiwuApplication.class, args);
+    }
+
+}
---
+++ yiwu/src/test/java/com/hellojava/YiwuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiwuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
